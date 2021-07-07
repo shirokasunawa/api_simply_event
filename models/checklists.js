fefbe2bfea585ecfd3a728373e49031a70130207
shirokasunawa <61684977+shirokasunawa@users.noreply.github.com>
2021-07-07 19:54:47 +0200

    checklist en cours

--- models/checklists.js
+++ models/checklists.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const checklistSchema = mongoose.Schema({
     titreCheclist : { type: String, required: false },
+    prixCheclist : { type: String, required: false },
+    quantiteCheclist : { type: String, required: false },
+    lieuCheclist : { type: String, required: false },
     productChecklist:{type: mongoose.Schema.Types.ObjectId, ref: 'products',required: false},
     created:{type: mongoose.Schema.Types.ObjectId, ref: 'event',required: false}
    
