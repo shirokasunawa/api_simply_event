d65f1fd60456842297d571344ccdb988a6c5f8be
shirokasunawa <61684977+shirokasunawa@users.noreply.github.com>
2021-07-13 13:10:11 +0200

    pub click ok

--- routes/router.js
+++ routes/router.js
@@ -1353,3 +1353,27 @@ router.get('/pub/:id', async (req,res)=>{
                 res.json({message : error.message})
             }
         })
+        router.get('/pubmodal', async (req,res)=>{
+            //res.send('Hello')
+            try{
+                const pub  = await Pub.find({
+                    "typeAbo":"nbrclick",
+                     "nbrClickReel": { $lte: 2} 
+                })
+                .populate({
+                  
+                        path:'img',
+                        model: imgModel ,
+                        select: ['img']
+                    
+                })
+                .exec(function(err, pub) {
+                    res.json(pub);
+                    // do something
+                });        
+               
+        
+            } catch(error) {
+                res.json({message : error.message})
+            }
+        })
